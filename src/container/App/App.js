6ebf9aaa81aa0d0cfc3fa4f9b3cde03a583de336
Jakob Holten <jakob.holten@macio.de>
2022-11-16 16:07:16 +0100

    Added material ui and code strutcture

---
+++ src/container/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Container from '@mui/material/Container';
+
+function App() {
+  return (
+    <div className="App">
+        <Container maxWidth="sm">
+          
+        </Container>
+    </div>
+  );
+}
+
+export default App;
